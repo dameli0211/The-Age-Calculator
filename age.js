e7b65082c2a47f808d430bead29b94ede7b36035
dameli0211 <dameli0211@gmail.com>
2021-03-02 14:23:01 -0500

    Declaring Variables and concatinating them

---
+++ age.js
@@ -0,0 +1,6 @@
+var currentYear = 2021;
+var possibleBirthYear = 1960;
+var possibleBirthYear = 1961
+var possibleAge1 = 61;
+var possibleAge2= 60;
+console.log("He is either " + possibleAge1 + " or " + possibleAge2 + " years old.");
